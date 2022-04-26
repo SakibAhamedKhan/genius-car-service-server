ee7405a86ba6b13f0d34937a3deb3fb2eeb9949c
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2022-04-26 21:55:17 +0600

    first commit

---
+++ __Steps.js
@@ -0,0 +1,13 @@
+/**
+ * ---------------------
+ * ONE TIME for your computer
+ * ---------------------
+ * 1. Create heroku account
+ * 2. Verify email
+ * 3. Install heroku Cli
+ * 4. heroku login
+ * --------------------
+ * For each	project one time
+ * --------------------
+ * 1. heroku create
+ */
